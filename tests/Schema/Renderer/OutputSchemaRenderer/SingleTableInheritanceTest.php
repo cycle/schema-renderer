28274c611b3b0baeb9482d73ee0be0fa3060869c
kastahov <kastahov1021@gmail.com>
2022-12-06 23:08:59 +0200

    Rollback SingleTableInheritanceTest

--- tests/Schema/Renderer/OutputSchemaRenderer/SingleTableInheritanceTest.php
+++ tests/Schema/Renderer/OutputSchemaRenderer/SingleTableInheritanceTest.php
@@ -52,7 +52,6 @@ final class SingleTableInheritanceTest extends BaseTest
                0 -> id -> int
                1 -> email
                2 -> balance -> float
-               _type -> _type
     Relations: not defined
 
 
