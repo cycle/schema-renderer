c6353936746bbda17c698495500d0f0155df7899
kastahov <kastahov1021@gmail.com>
2022-12-08 22:21:24 +0200

    Fix

--- src/MermaidRenderer/Schema/ClassDiagram.php
+++ src/MermaidRenderer/Schema/ClassDiagram.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace Cycle\Schema\Renderer\MermaidRenderer\Schema;
 
 use Cycle\Schema\Renderer\MermaidRenderer\Entity\EntityInterface;
-use Cycle\Schema\Renderer\MermaidRenderer\Stringable;
 
-final class ClassDiagram implements SchemaInterface, Stringable
+final class ClassDiagram implements SchemaInterface
 {
     private const DIAGRAM = <<<DIAGRAM
 
--- src/MermaidRenderer/Schema/SchemaInterface.php
+++ src/MermaidRenderer/Schema/SchemaInterface.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Cycle\Schema\Renderer\MermaidRenderer\Schema;
 
 use Cycle\Schema\Renderer\MermaidRenderer\Entity\EntityInterface;
+use Cycle\Schema\Renderer\MermaidRenderer\Stringable;
 
-interface SchemaInterface
+interface SchemaInterface extends Stringable
 {
     public function addEntity(EntityInterface $entity): void;
 }
